1b59ff8f106db8ab6b5cde80d9d38249bb447bfe
Smah-amen <samahamen20001@gmail.com>
2025-10-30 23:45:43 +0200

    feat: add initial component setup with button and utility functions
    - Created components.json for component configuration.
    - Implemented Button component with variants and sizes using class-variance-authority.
    - Added utility function  for merging class names.
    - Updated next.config.ts to enable turbopack.
    - Added necessary dependencies in package.json and package-lock.json.
    - Removed unused SVG files and added new assets including logo and dashboard images.

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  // enable turbopack with an options object (adjust properties as needed)
+  turbopack: {
+
+  },
   /* config options here */
 };
 
