ac5a8c2abf0a138b501341746dc4d5f8cfb0ed93
Smah-amen <samahamen20001@gmail.com>
2025-11-03 21:56:25 +0200

    feat: add UserDropdown component with avatar and sign-out functionality

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -1,3 +1,4 @@
+'use client'
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -6,10 +7,30 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import {
+  Avatar,
+  AvatarFallback,
+  AvatarImage,
+} from "@/components/ui/avatar"
+import { useRouter } from "next/navigation"
+import { Button } from "./ui/button"
 const UserDropdown = () => {
+  const router = useRouter()
+  const handelSignOut = async() => {
+    router.push('/signin')
+  }
+  const user = { name: "sam" , email: "sam@gmail.com"}
   return (
 <DropdownMenu>
-  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuTrigger  asChild>
+    <Button variant="ghost" className="flex items-center gap-3 text-gray-400 hover:text-yellow-500">
+    <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+    </Button>
+
+  </DropdownMenuTrigger>
   <DropdownMenuContent>
     <DropdownMenuLabel>My Account</DropdownMenuLabel>
     <DropdownMenuSeparator />
