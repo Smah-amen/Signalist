c55d3dca55c35d7e152186ddec8252d6805d6260
Smah-amen <samahamen20001@gmail.com>
2025-11-03 20:55:01 +0200

    chore: add @radix-ui/react-dropdown-menu dependency and update package-lock.json

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image"
 import Link from "next/link"
 import NavItems from "./NavItems"
+import UserDropdown from "./UserDropdown"
 
 const Header = () => {
   return (
@@ -12,6 +13,7 @@ const Header = () => {
             <nav className="hidden sm:block">
                 <NavItems/>
             </nav>
+            <UserDropdown/>
         </div>
       
     </header>
